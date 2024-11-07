ff9edc0a1155e94e4068d0c06f40500444a97d13
metaproject1805 <metataskinternational@gmail.com>
2024-11-07 21:57:50 +0100

    withdrawal wallet_id set to 200

--- withdrawals/models.py
+++ withdrawals/models.py
@@ -13,7 +13,7 @@ class Withdrawal(models.Model):
   ]
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="eligible_for_withdrawal" )
   wallet_address = models.CharField(max_length=200, null=True, blank=True)
-  payment_status = models.CharField(max_length=20, choices=PAYMENT_STATUS_CHOICES, default="Pending")
+  payment_status = models.CharField(max_length=200, choices=PAYMENT_STATUS_CHOICES, default="Pending")
   wallet_email = models.EmailField(null=True, blank=True)
   wallet_id = models.CharField(max_length=20, null=True, blank=True)
   amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
